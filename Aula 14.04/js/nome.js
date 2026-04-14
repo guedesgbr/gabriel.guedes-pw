d8afcd7b40afaf7c3671b40090174f621a50b7ec
Gabriel Guedes <gabrielroguedes@gmail.com>
2026-04-14 08:06:55 -0300

    Add files via upload

---
+++ Aula 14.04/js/nome.js
@@ -0,0 +1,10 @@
+var nome = "Gabriel"
+var nome2 = prompt("Digite seu nome: ")
+var idade = parseInt(prompt("Digite sua idade: "))
+
+
+
+console.log("Seu nome é: " , nome);
+console.log("Sua idade é: " , nome)
+alert("Seu nome é: " + nome2)
+alert("Sua idade é: " + idade)
---
+++ Aula 14.04/js/soma.js
@@ -0,0 +1,4 @@
+var num1 = parseFloat(prompt("Digite o 1º Número: "));
+var num2 = parseFloat(prompt("Digite o 2º Número: "));
+
+console.log("A soma de ambos é igual a: ", (num1 + num2));
