a33cc719e927947c1da0dd7ab4fb0454ceea6af9
Gabriel Guedes <gabrielroguedes@gmail.com>
2026-05-05 08:32:23 -0300

    Add files via upload

---
+++ Aula 05.05/js/script.js
@@ -0,0 +1,32 @@
+    //Estrutura de Repetição While
+    var x = 0
+    var num = 0
+while(x < 5){
+
+    x++
+    num = x + num;
+
+ console.log("Testando repetição"+ x);
+}
+
+
+// criar um array passando por todos os elementos
+var arr =['teste 1', 'teste2', 'teste 3', 'a' ];
+
+var y =0;
+//passando por todos os elementos
+while (y <=3){
+
+    console.log(arr[y]);
+    y++
+}
+
+// passando por todas as palavras
+var palavra = "GABRIEL";
+var i =0;
+// passando por todos elementos
+while(i < 7){
+
+    console.log(palavra[i]);
+    i++
+}
